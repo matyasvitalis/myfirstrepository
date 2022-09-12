942e5837243b67460387e04080560353bfe650ff
matyasvitalis <info@vmatthias.hu>
2022-09-12 15:25:47 +0200

    I added "Animals are cute" text to the code.

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,6 @@
 public class Animal {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");}
+        System.out.println("Hello world!");
+    System.out.println("Animals are cute!");}
 }
